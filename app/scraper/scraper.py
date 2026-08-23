fe73566c610127b58134eac06c5c6a6802765c40
Muhammad Atif <atifwazir45@gmail.com>
2026-08-23 15:40:46 +0500

    Add scraper error handling

--- app/scraper/scraper.py
+++ app/scraper/scraper.py
@@ -1,33 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
-books_data = []
-def scrap_page(url):
-    response = requests.get(url,timeout=10)
-    response.raise_for_status()
+def scrape_page(url):
+    page_books = []
+    try:
+        response = requests.get(url,timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Re  uest failed {url}")
+        print(e)
+        return []    
     html = response.text
     soup = BeautifulSoup(html,"html.parser")
     books = soup.find_all("li",class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
     for book in books:
-        tag = book.find("h3")
-        title = tag.a["title"]
+        title_tag = book.find("h3")
+        if title_tag is None:
+            print("skiping book,Title not found")
+            continue
+        title = title_tag.a["title"]
         price_tag = book.find("p",class_="price_color")
+        if price_tag is None:
+            print("skiping book,Price not found")
+            continue
         price = price_tag.get_text(strip=True)
         price = price.replace("Â£","")
         price = float(price)
         rating_tag = book.find("p",class_= "star-rating")
+        if rating_tag is None:
+            print("Skipping book : Rating tag not found")
+            continue
         rating = rating_tag["class"]
         rating = rating[1]
         data ={
             "title":title,
             "price":price,
             "rating":rating }
-        books_data.append(data)
+        page_books.append(data)
+    return page_books    
 def main():
+    books_data = []
     url_1 = "https://books.toscrape.com/"
-    scrap_page(url_1)
+    books = scrape_page(url_1)
+    books_data.extend(books)
     for page_no in range(2,51):
         url_2 = f"https://books.toscrape.com/catalogue/page-{page_no}.html"
-        scrap_page(url_2)
+        books = scrape_page(url_2)
+        books_data.extend(books)
     print(len(books_data))
 main()        
 
