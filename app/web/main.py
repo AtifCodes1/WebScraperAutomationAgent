3c1a44d80fad9138c3b11dffb20b9cdae77dd10d
Muhammad Atif <atifwazir45@gmail.com>
2026-08-30 15:51:49 +0500

    Connect frontend to fastapi

--- app/web/main.py
+++ app/web/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+templates = Jinja2Templates(directory="app/templates")
 app = FastAPI(
     title="Web Scraper Automation Platform",
     description="A web scraping and data automation platform",
     version="1.0.0"
 )
-@app.get("/")
-def moggli():
-    return {"Pa khair raghlay"}
+@app.get("/",response_class=HTMLResponse)
+def moggli(request : Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"request" : request}
+    )
